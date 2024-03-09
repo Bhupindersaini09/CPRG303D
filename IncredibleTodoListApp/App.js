3f37d8c987014d77d0270e34abee3536240cca3e
Bhupinder <bhupindersaini0699@gmail.com>
2024-03-08 21:01:24 -0700

    lab-3

--- IncredibleTodoListApp/App.js
+++ IncredibleTodoListApp/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useState } from "react";
+import { SafeAreaView } from "react-native";
+import ToDoList from "./Components/ToDoList";
+import ToDoForm from "./Components/ToDoForm";
 
-export default function App() {
+function App() {
+  const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ToDoList tasks={tasks} />
+      <ToDoForm />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
